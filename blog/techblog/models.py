0f6a95edea5b2aefd52a28cd7a8d883909512ee9
Teddy Juma <tedd.juma@outlook.com>
2018-12-26 16:39:05 +0300

    Done some tests, Failed. Fix the bug later

--- blog/techblog/models.py
+++ blog/techblog/models.py
@@ -1,3 +1,69 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    #TODO: Make sure the passwords are hashed
+    password = models.CharField(max_length=64)
+    email_address = models.CharField(max_length=200)
+
+class Author(models.Model):
+    first_name = models.CharField(max_length = 20)
+    last_name = models.CharField(max_length = 20)
+    description = models.TextField()
+
+    #avatar_name: path to image
+    avatar = models.CharField(max_length=200, null=True)
+    email_address = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ["first_name"]
+    
+    def __str__(self):
+        return "FName:{}, LName {}".format(self.first_name, self.last_name)
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.title = models.CharField(max_length=64)
+    slug_field = models.CharField(max_length=200, null=True, blank=True)
+    content = models.TextField()
+    publish_status = models.BooleanField(default=False)
+    publish_date = models.DateTimeField(blank=True, null=True)
+    
+    class Meta:
+        ordering = ["publish_date"]
+
+    def __str__(self):
+        return "{} {} \n{}".format(self.title, self.content, self.publish_status)
+
+    def publish(self):
+        self.publish_date = timezone.now()
+        self.publish_status = True
+
+        self.save
+    
+    def generate_slug_field(self):
+        if self.title:
+            title = self.title
+        else: 
+            return
+
+        slug = title.lower().replace(" ", "_")
+        self.slug_field = slug
+
+        self.save()
+
+class Comment(models.Model):
+    comment_post_ID = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_date = models.DateTimeField()
+    comment_content = models.TextField()
+
+class Like(models.Model):
+    like_IP = models.CharField(max_length = 20)
+    liked_post = models.ForeignKey(Post, on_delete=models.CASCADE, blank=True, null=True)
+    liked_comment = models.ForeignKey(Comment, on_delete=models.CASCADE, blank=True, null=True)
+class Tag(models.Model):
+    tag_name = models.CharField(max_length= 15)
+    post_taged = models.ForeignKey(Post, on_delete=models.CASCADE)
--- blog/techblog/tests.py
+++ blog/techblog/tests.py
@@ -1,3 +1,76 @@
 from django.test import TestCase
-
+from django.utils import timezone
+from .models import *
 # Create your tests here.
+
+class PostsTests(TestCase):
+
+    def setUp(self):
+        self.dummy_user = User.objects.create(
+                username = "Foo",
+                password = "Bar",
+                email_address = "bar@foo.com"
+                )
+
+        self.dummy_author = Author.objects.create(
+                first_name = "Foo",
+                last_name = "Bar",
+                description = "Foo went to a Bar and bought a beer",
+                email_address=  "foo@bar.com"
+                )
+
+        self.dummy_post = Post.objects.create(
+                author = self.dummy_author,
+                title = "Foo went to a Bar",
+                content = "Foo went to a Bar and bought a beer",
+                publish_status = False,
+                publish_date = timezone.now()
+                ) 
+
+        self.dummy_comment = Comment.objects.create(
+                comment_post_ID = self.dummy_post,
+                comment_author = self.dummy_user,
+                comment_date = timezone.now(),
+                comment_content = "Hello I am Foo, and i didnt go to the bar last night",
+                )
+
+        self.dummy_post_like = Like.objects.create(
+                like_IP = "127.0.0.1",
+                liked_post = self.dummy_post
+                )
+
+        self.dummy_comment_like = Like.objects.create(
+                like_IP = "127.0.0.1",
+                liked_comment = self.dummy_comment,
+                )
+
+        self.dummy_tag = Tag.objects.create(
+                tag_name = "Foo",
+                post_taged = self.dummy_post,
+                )
+
+        self.dummy_tag_2 = Tag.objects.create(
+                tag_name = "Bar",
+                post_taged = self.dummy_post,
+                )
+
+
+    def test_post_publication(self):
+        """
+            Test if publication method works
+        """
+        self.dummy_post.publish()
+        self.assertTrue(self.dummy_post.publish_status)
+
+    def test_post_author(self):
+        """
+            Test Author
+        """
+        self.assertEqual(self.dummy_post.author, self.dummy_author)
+
+    def test_slug_field_generation(self):
+        """
+            Test generation of the slug field, useful with urls
+        """
+        self.dummy_post.generate_slug_field()
+        self.assertEqual(self.dummy_post.slug_field,"foo_went_to_a_bar" )
